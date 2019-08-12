098243eca60f2051105b409e14ac555d61d3d6a3
Vo Anh Duy <davo@thoughtworks.com>
2019-08-12 16:23:09 +0800

    Add liskov-substitution

---
+++ solid/liskov-substitution/src/main/java/vehicle/BaseVehicle.java
@@ -0,0 +1,19 @@
+package vehicle;
+
+public abstract class BaseVehicle {
+    public int getSpeed() {
+        return speed;
+    }
+
+    private int speed = 0;
+
+    protected abstract int maxSpeed();
+
+    public void accelerate() {
+        if (speed < maxSpeed()) speed += 1;
+    }
+
+    public void decelerate() {
+        if (speed > 0) speed -= 1;
+    }
+}
---
+++ solid/liskov-substitution/src/main/java/vehicle/Helicopter.java
@@ -0,0 +1,29 @@
+package vehicle;
+
+public class Helicopter extends BaseVehicle {
+
+    protected int maxSpeed() {
+        return 250;
+    }
+
+    private boolean takenOff = false;
+
+    public void takeOff() {
+        takenOff = true;
+    }
+
+    public void land() {
+        if (getSpeed() > 0) {
+            throw new RuntimeException("Cannot land while moving!");
+        }
+        takenOff = false;
+    }
+
+    @Override
+    public void accelerate() {
+        if (!takenOff) {
+            throw new RuntimeException("Can only accelerate once Taken Off.");
+        }
+        super.accelerate();
+    }
+}
